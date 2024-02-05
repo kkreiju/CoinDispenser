23e1b59024475bbff57efad26057e9d078a9eeed
kkreiju <138178348+kkreiju@users.noreply.github.com>
2024-02-05 11:36:42 +0800

    Add files via upload

---
+++ CoinDispenser/CoinDispenser/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace CoinDispenser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ChangeLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.PurchasedTextbox = new System.Windows.Forms.TextBox();
+            this.TenderedTextbox = new System.Windows.Forms.TextBox();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.TenPesoLabel = new System.Windows.Forms.Label();
+            this.FivePesoLabel = new System.Windows.Forms.Label();
+            this.OnePesoLabel = new System.Windows.Forms.Label();
+            this.TwentyFiveCentLabel = new System.Windows.Forms.Label();
+            this.TenCentLabel = new System.Windows.Forms.Label();
+            this.FiveCentLabel = new System.Windows.Forms.Label();
+            this.OneCentLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount Purchased";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(112, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Amount Tendered";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(112, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Change";
+            // 
+            // ChangeLabel
+            // 
+            this.ChangeLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.ChangeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ChangeLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangeLabel.Location = new System.Drawing.Point(247, 102);
+            this.ChangeLabel.Name = "ChangeLabel";
+            this.ChangeLabel.Size = new System.Drawing.Size(96, 22);
+            this.ChangeLabel.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(136, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ten-Peso Coin";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(136, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Five-Peso Coin";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(136, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 18);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "One-Peso";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(136, 241);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 18);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "25-Cent";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(136, 266);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 18);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "10-Cent";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(136, 289);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 18);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "5-Cent";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(136, 315);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 18);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "1-Cent";
+            // 
+            // PurchasedTextbox
+            // 
+            this.PurchasedTextbox.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PurchasedTextbox.Location = new System.Drawing.Point(247, 16);
+            this.PurchasedTextbox.Name = "PurchasedTextbox";
+            this.PurchasedTextbox.Size = new System.Drawing.Size(100, 21);
+            this.PurchasedTextbox.TabIndex = 18;
+            // 
+            // TenderedTextbox
+            // 
+            this.TenderedTextbox.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TenderedTextbox.Location = new System.Drawing.Point(247, 62);
+            this.TenderedTextbox.Name = "TenderedTextbox";
+            this.TenderedTextbox.Size = new System.Drawing.Size(100, 21);
+            this.TenderedTextbox.TabIndex = 19;
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.DisplayButton.Font = new System.Drawing.Font("Viner Hand ITC", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayButton.Location = new System.Drawing.Point(369, 19);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(96, 61);
+            this.DisplayButton.TabIndex = 20;
+            this.DisplayButton.Text = "Display";
+            this.DisplayButton.UseVisualStyleBackColor = false;
+            this.DisplayButton.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // TenPesoLabel
+            // 
+            this.TenPesoLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.TenPesoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TenPesoLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TenPesoLabel.Location = new System.Drawing.Point(245, 170);
+            this.TenPesoLabel.Name = "TenPesoLabel";
+            this.TenPesoLabel.Size = new System.Drawing.Size(96, 18);
+            this.TenPesoLabel.TabIndex = 21;
+            // 
+            // FivePesoLabel
+            // 
+            this.FivePesoLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.FivePesoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FivePesoLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FivePesoLabel.Location = new System.Drawing.Point(245, 193);
+            this.FivePesoLabel.Name = "FivePesoLabel";
+            this.FivePesoLabel.Size = new System.Drawing.Size(96, 19);
+            this.FivePesoLabel.TabIndex = 22;
+            // 
+            // OnePesoLabel
+            // 
+            this.OnePesoLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.OnePesoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OnePesoLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OnePesoLabel.Location = new System.Drawing.Point(245, 217);
+            this.OnePesoLabel.Name = "OnePesoLabel";
+            this.OnePesoLabel.Size = new System.Drawing.Size(96, 19);
+            this.OnePesoLabel.TabIndex = 23;
+            // 
+            // TwentyFiveCentLabel
+            // 
+            this.TwentyFiveCentLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.TwentyFiveCentLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TwentyFiveCentLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TwentyFiveCentLabel.Location = new System.Drawing.Point(245, 240);
+            this.TwentyFiveCentLabel.Name = "TwentyFiveCentLabel";
+            this.TwentyFiveCentLabel.Size = new System.Drawing.Size(96, 19);
+            this.TwentyFiveCentLabel.TabIndex = 24;
+            // 
+            // TenCentLabel
+            // 
+            this.TenCentLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.TenCentLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TenCentLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TenCentLabel.Location = new System.Drawing.Point(245, 266);
+            this.TenCentLabel.Name = "TenCentLabel";
+            this.TenCentLabel.Size = new System.Drawing.Size(96, 18);
+            this.TenCentLabel.TabIndex = 25;
+            // 
+            // FiveCentLabel
+            // 
+            this.FiveCentLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.FiveCentLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FiveCentLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiveCentLabel.Location = new System.Drawing.Point(245, 289);
+            this.FiveCentLabel.Name = "FiveCentLabel";
+            this.FiveCentLabel.Size = new System.Drawing.Size(96, 18);
+            this.FiveCentLabel.TabIndex = 26;
+            // 
+            // OneCentLabel
+            // 
+            this.OneCentLabel.BackColor = System.Drawing.Color.LightCyan;
+            this.OneCentLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OneCentLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OneCentLabel.Location = new System.Drawing.Point(245, 314);
+            this.OneCentLabel.Name = "OneCentLabel";
+            this.OneCentLabel.Size = new System.Drawing.Size(96, 19);
+            this.OneCentLabel.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(112, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 14);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Coins";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(528, 393);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.OneCentLabel);
+            this.Controls.Add(this.FiveCentLabel);
+            this.Controls.Add(this.TenCentLabel);
+            this.Controls.Add(this.TwentyFiveCentLabel);
+            this.Controls.Add(this.OnePesoLabel);
+            this.Controls.Add(this.FivePesoLabel);
+            this.Controls.Add(this.TenPesoLabel);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.TenderedTextbox);
+            this.Controls.Add(this.PurchasedTextbox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ChangeLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ChangeLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox PurchasedTextbox;
+        private System.Windows.Forms.TextBox TenderedTextbox;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.Label TenPesoLabel;
+        private System.Windows.Forms.Label FivePesoLabel;
+        private System.Windows.Forms.Label OnePesoLabel;
+        private System.Windows.Forms.Label TwentyFiveCentLabel;
+        private System.Windows.Forms.Label TenCentLabel;
+        private System.Windows.Forms.Label FiveCentLabel;
+        private System.Windows.Forms.Label OneCentLabel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CoinDispenser/CoinDispenser/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace CoinDispenser
+{
+    public partial class Form1 : Form
+    {
+        double AmountPurchased, AmountTendered, ChangeAmount;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayButton_Click(object sender, EventArgs e)
+        {
+            AmountPurchased = Convert.ToDouble(PurchasedTextbox.Text);
+            AmountTendered = Convert.ToDouble(TenderedTextbox.Text);
+            ChangeAmount = AmountTendered - AmountPurchased;
+            ChangeLabel.Text = ChangeAmount.ToString();
+            TenPesoLabel.Text = ((int) (ChangeAmount / 10)).ToString();
+            FivePesoLabel.Text = ((int) (ChangeAmount % 10 / 5)).ToString();
+            OnePesoLabel.Text = ((int) (ChangeAmount % 10 % 5 / 1)).ToString();
+            TwentyFiveCentLabel.Text = ((int) (ChangeAmount % 10 % 5 % 1 / .25)).ToString();
+            TenCentLabel.Text = ((int) (ChangeAmount % 10 % 5 % 1 % .25 /.1)).ToString();
+            FiveCentLabel.Text = ((int) (ChangeAmount % 10 % 5 % 1 % .25 % .10 / .05)).ToString();
+            OneCentLabel.Text = Convert.ToInt16((ChangeAmount % 10 % 5 % 1 % .25 % .10 % .05 / .01)).ToString();
+        }
+    }
+}
